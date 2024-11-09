dbcab65517dc25c6c314e17ec4149394273d8975
Italo Magno <italomagno10@gmail.com>
2024-11-08 22:15:38 -0300

    finishing...

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,81 @@
-export interface Figure {}
+export interface Figure {
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  shape: 'triangle' = 'triangle';
 
-export class Circle implements Figure {}
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    this.color = color;
 
-export class Rectangle implements Figure {}
+    const ab = a + b;
+    const bc = b + c;
+    const ac = a + c;
+
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('any length is <= 0');
+    }
+
+    if (a >= bc || b >= ac || c >= ab) {
+      throw new Error('it is not a triangle');
+    }
+  }
+
+  getArea(): number {
+    const p = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export class Circle implements Figure {
+  shape: 'circle' = 'circle';
+
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public r: number,
+  ) {
+    this.color = color;
+
+    if (r <= 0) {
+      throw new Error('this is a dot');
+    }
+  }
+
+  getArea(): number {
+    const area = Math.PI * Math.pow(this.r, 2);
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  shape: 'rectangle' = 'rectangle';
+
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public a: number,
+    public b: number,
+  ) {
+    if (a <= 0 || b <= 0) {
+      throw new Error('any length is <= 0');
+    }
+    this.color = color;
+  }
+
+  getArea(): number {
+    return this.a * this.b;
+  }
+}
 
 export function getInfo(figure): string {
-  return typeof figure;
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
